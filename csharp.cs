a70c5002ca0bce0ae4c4702a3e33276e6fc5977e
pwang86 <pwang86@gmail.com>
2021-06-23 11:28:51 +1000

    design linked list in c sharp

--- csharp.cs
+++ csharp.cs
@@ -77,3 +77,72 @@ public class Solution {
         return dummy.next;
     } 
 } 
+
+// Deisgn Linked List
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int value) {
+        this.val = value;
+    }
+}
+public class MyLinkedList {
+    
+    private ListNode _head;
+    private int _size;
+    /** Initialize your data structure here. */
+    public MyLinkedList() {
+        _head = new ListNode(0);
+        _size = 0;
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int Get(int index) {
+        if (index >= _size || index < 0)
+            return -1;
+        ListNode cur = _head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void AddAtHead(int val) {
+        AddAtIndex(0, val);
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    public void AddAtTail(int val) {
+        AddAtIndex(_size, val);
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void AddAtIndex(int index, int val) {
+        if (index > _size)
+            return;
+        if (index < 0)
+            index = 0;
+        _size++;
+        ListNode pre = _head;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = new ListNode(val);
+        cur.next = pre.next;
+        pre.next = cur;
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void DeleteAtIndex(int index) {
+       if (index < 0 || index >= _size) {
+           return;
+        }
+        _size--;
+        ListNode pre = _head;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        pre.next = pre.next.next;
+    }
+}
