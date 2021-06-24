f39880a3897d2a212cd2cae3def5c69dc9e252f9
pwang86 <pwang86@gmail.com>
2021-06-24 11:46:55 +1000

    reverse linked list in c sharp

--- csharp.cs
+++ csharp.cs
@@ -75,6 +75,21 @@ public class Solution {
         }
             
         return dummy.next;
+    }
+
+    // reverse linked list
+    public ListNode ReverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        ListNode tmp = null;
+        
+        while (cur != null) {
+            tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
     } 
 } 
 
