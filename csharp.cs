c0bb3f6cf00ec8ec82e328004f87af034ca6306c
pwang86 <pwang86@gmail.com>
2021-06-27 09:59:48 +1000

    intersection of two linked lists in csharp

--- csharp.cs
+++ csharp.cs
@@ -131,6 +131,48 @@ public class Solution {
         
         return dummy.next;
     } 
+
+    // intersetion of two linked lists
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        
+        int lenA = 0, lenB = 0;
+        while (curA != null) {
+            lenA++;
+            curA = curA.next;
+        }
+        while (curB != null) {
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        
+        if (lenB > lenA) {
+            int tmp = lenB;
+            lenB = lenA;
+            lenA = tmp;
+            ListNode tmpNode = curB;
+            curB = curA;
+            curA = tmpNode;
+        }
+        
+        int gap = lenA - lenB;
+        while (gap > 0) {
+            gap--;
+            curA = curA.next;
+        }
+        
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
 } 
 
 // Deisgn Linked List
