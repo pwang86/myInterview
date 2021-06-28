4f83bef5eb074e7f970c303460c6ae2c5bdc1e64
pwang86 <pwang86@gmail.com>
2021-06-28 13:58:42 +1000

    linked list cycle ii in c sharp

--- csharp.cs
+++ csharp.cs
@@ -173,6 +173,27 @@ public class Solution {
         }
         return null;
     }
+
+    // linked list cycle II
+    public ListNode DetectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index2;
+            }
+        }
+        return null;
+    }
 } 
 
 // Deisgn Linked List
