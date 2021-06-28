afe3a1a476e1973a7364f30b6a4aabe84d81374f
pwang86 <pwang86@gmail.com>
2021-06-28 13:58:49 +1000

    linked list cycle ii in js

--- javascript.js
+++ javascript.js
@@ -179,6 +179,29 @@ var removeElements = function (head, val) {
     }
     return null;
 };
+// linked list cycle II
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var detectCycle = function(head) {
+    let fast = head;
+    let slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast == slow) {
+            let index1 = fast;
+            let index2 = head;
+            while (index1 != index2) {
+                index1 = index1.next;
+                index2 = index2.next;
+            }
+            return index1;
+        }
+    }
+    return null;
+};
 
 // Design Linked List
 class ListNode {
