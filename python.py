9bb22da468946945e1c3397331550b728fc4df0a
pwang86 <pwang86@gmail.com>
2021-06-29 16:20:13 +1000

    valid anagram in python

--- python.py
+++ python.py
@@ -149,6 +149,21 @@ class Solution:
                 return index2 
         return None
 
+    # valid anagram
+     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        record = [0] * 26
+        for i in s:
+            record[ord(i) - ord('a')] += 1
+        for i in t:
+            record[ord(i) - ord('a')] -= 1
+            
+        for i in range(26):
+            if record[i] != 0:
+                return False
+        return True
+
 # Deisgn Linked List
 class ListNode:
     def __init__(self, val):
