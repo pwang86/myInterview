2d2c7d481b4ae91cd39ddc83c9aa39b1c5358461
pwang86 <pwang86@gmail.com>
2021-06-30 16:31:43 +1000

    intersection of two arrays in js

--- javascript.js
+++ javascript.js
@@ -227,6 +227,27 @@ var removeElements = function (head, val) {
     return true;
 };
 
+// intersection of two arrays
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+ var intersection = function(nums1, nums2) {
+    if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+        return [];
+    }
+    let set1 = new Set(nums1);
+    let res = new Set();
+
+    for (let i of nums2) {
+        if (set1.has(i)) {
+            res.add(i);
+        }
+    }
+    return Array.from(res);
+};
+
 // Design Linked List
 class ListNode {
     constructor(val, next) {
