1328284c8fc80a4b54bb0e96d6c0c105b7c955d3
pwang86 <pwang86@gmail.com>
2021-06-30 16:32:00 +1000

    intersection of two arrays in python

--- python.py
+++ python.py
@@ -163,6 +163,15 @@ class Solution:
             if record[i] != 0:
                 return False
         return True
+    
+    # intersection of two arrays
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = set()
+        set1 = set(nums1)
+        for n in nums2:
+            if n in set1:
+                res.add(n)
+        return list(res)
 
 # Deisgn Linked List
 class ListNode:
