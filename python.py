4effb294619be7714e6a344ad4e72c28fa6e80c9
pwang86 <pwang86@gmail.com>
2021-07-05 11:49:01 +1000

    3 sum in python

--- python.py
+++ python.py
@@ -237,6 +237,36 @@ class Solution:
             else:
                 return False
         return True
+    
+    # 3 sum
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                return res
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            
+            left = i + 1
+            right = len(nums) - 1
+            while right > left:
+                tmp = nums[i] + nums[left] + nums[right]
+                if tmp > 0:
+                    right -= 1
+                elif tmp < 0:
+                    left += 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while right > left and nums[right] == nums[right - 1]:
+                        right -= 1
+                    while right > left and nums[left] == nums[left + 1]: 
+                        left += 1
+                    right -= 1
+                    left += 1
+                
+        return res
 
 # Deisgn Linked List
 class ListNode:
