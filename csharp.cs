31452e19a172072da57ff3dde8914f85cf23f595
pwang86 <pwang86@gmail.com>
2021-07-10 18:36:45 +1000

    use kmp in strStr in c sharp

--- csharp.cs
+++ csharp.cs
@@ -410,11 +410,41 @@ public class Solution {
     }
 
     // implement strStr()
-    public int StrStr(string haystack, string needle) {
-        if (needle ==""){
-            return 0;             
+    public void GetNext(int[] next, string s) {
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i < s.Length; i++) {
+            while (j >= 0 && s[i] != s[j + 1]) {
+                j = next[j];
+            }
+            if (s[i] == s[j + 1]) {
+                j++;
+            }
+            next[i] = j;
         }
-        return haystack.IndexOf(needle);
+    }
+    // get next
+    public int StrStr(string haystack, string needle) {
+        if (needle.Length == 0) {
+            return 0;
+        }
+        int[] next = new int[needle.Length];
+        GetNext(next, needle);
+
+        int j = -1;
+        for (int i = 0; i< haystack.Length; i++) {
+            while (j >= 0 && haystack[i] != needle[j + 1]) {
+                j = next[j];
+            }
+            if (haystack[i] == needle[j + 1]) {
+                j++;
+            }
+            if (j == needle.Length - 1) {
+                return i - needle.Length + 1;
+            }
+        }
+        
+        return -1;
     }
 } 
 
