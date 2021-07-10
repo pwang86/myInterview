8959d12e7474f653c5749767605844ab45e6f8d0
pwang86 <pwang86@gmail.com>
2021-07-10 18:37:11 +1000

    use kmp in strStr in js

--- javascript.js
+++ javascript.js
@@ -452,10 +452,37 @@ function getSum(n) {
  * @return {number}
  */
  var strStr = function(haystack, needle) {
-    if (needle == "")
-        return 0;
-    return haystack.indexOf(needle);
+    if (needle.length == 0) return 0;
+    let next = new Array(needle.length);
+    getNext(next, needle);
+    let j = -1;
+    for (let i = 0; i < haystack.length; i++) {
+        while (j >= 0 && haystack[i] != needle[j + 1]) {
+            j = next[j];
+        }
+        if (haystack[i] == needle[j + 1]) {
+            j++;
+        }
+        if (j == needle.length - 1) {
+            return (i - needle.length + 1);
+        }
+    }
+    return -1;
 };
+
+function getNext(next, s) {
+    let j = -1;
+    next[0] = j;
+    for (let i = 1; i < s.length; i++) {
+        while (j >= 0 && s[i] != s[j + 1]) {
+            j = next[j];
+        }
+        if (s[i] == s[j + 1]) {
+            j++;
+        }
+        next[i] = j;
+    }
+}
 
 // Design Linked List
 class ListNode {
