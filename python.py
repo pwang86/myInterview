cb654d87db871793e1783dea74216555655be527
pwang86 <pwang86@gmail.com>
2021-07-10 18:38:10 +1000

    use kmp in strStr in python

--- python.py
+++ python.py
@@ -325,12 +325,30 @@ class Solution:
     
     # implement strStr
     def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
+        if len(needle) == 0:
             return 0
-        try:
-            return haystack.index(needle)
-        except:
-            return -1
+        next = [0] * len(needle)
+        self.getNext(next, needle)
+        j = -1
+        for i in range(len(haystack)):
+            while j >= 0 and haystack[i] != needle[j + 1]:
+                j = next[j]
+            if haystack[i] == needle[j + 1]:
+                j += 1
+            if j == len(needle) - 1:
+                return i - len(needle) + 1
+        return -1
+        
+    def getNext(slef, next: list[int], s: str) -> None:
+        j = -1
+        next[0] = j
+        for i in range(1, len(s)):
+            while j >= 0 and s[i] != s[j + 1]:
+                j = next[j]
+            if s[i] == s[j + 1]:
+                j += 1
+            next[i] = j
+            
 
 # Deisgn Linked List
 class ListNode:
