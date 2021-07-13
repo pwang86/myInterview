158bf8966babd2e95745beb29bd6c3615b11cd79
pwang86 <pwang86@gmail.com>
2021-07-13 13:54:00 +1000

    implement queue using stacks in js

--- javascript.js
+++ javascript.js
@@ -557,6 +557,55 @@ function getNext(next, s) {
     }
 }
 
+// implement queue using stacks
+/**
+ * Initialize your data structure here.
+ */
+ var MyQueue = function() {
+    this.sIn = [];
+    this.sOut = [];
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.sIn.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if (this.sOut.length == 0) {
+        while (this.sIn.length != 0) {
+            this.sOut.push(this.sIn.pop());
+        }
+    }
+    return this.sOut.pop();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    let res = this.pop();
+    this.sOut.push(res);
+    return res;
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.sIn.length == 0 && this.sOut.length == 0;
+};
+
 // Design Linked List
 class ListNode {
     constructor(val, next) {
