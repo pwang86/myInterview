53975be39ba1b7efc4d1139be516eeef45c9e192
pwang86 <pwang86@gmail.com>
2021-08-21 21:18:36 +1000

    Merge Two Binary Trees in python

--- python.py
+++ python.py
@@ -976,6 +976,18 @@ class Solution:
         root.right = self.helper(nums[maxIndex + 1:])
         
         return root
+    
+    # Merge Two Binary Trees
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        
+        root = TreeNode(root1.val + root2.val)
+        root.left = self.mergeTrees(root1.left, root2.left)
+        root.right = self.mergeTrees(root1.right, root2.right)
+        return root
 
 # Sliding Window Maximum
 class MyQueue:
