d98d18495cdad10c756cb6d60641c4f8db7338a0
pwang86 <pwang86@gmail.com>
2021-08-22 11:47:51 +1000

    Search in a Binary Search Tree in js

--- javascript.js
+++ javascript.js
@@ -1215,6 +1215,20 @@ var buildTree = function(preorder, inorder) {
     return root;
 };
 
+// Search in a Binary Search Tree
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+ var searchBST = function(root, val) {
+    if (root == null || root.val == val)
+        return root;
+    if (root.val > val) return searchBST(root.left, val);
+    if (root.val < val) return searchBST(root.right, val);
+    
+    return null;
+};
 
 // implement queue using stacks
 /**
