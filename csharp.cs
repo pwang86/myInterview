f9d16ddd9f87b7c633e6b5b5a96b962903e6a2ce
pwang86 <pwang86@gmail.com>
2021-08-27 07:25:05 +1000

    Lowest Common Ancestor of a BST in c sharp

--- csharp.cs
+++ csharp.cs
@@ -1337,6 +1337,20 @@ public class Solution {
         if (left == null) return right;
         return left;
     }
+
+    // Lowest Common Ancestor of a Binary Search Tree
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
 } 
 
 // implement queue using stacks
