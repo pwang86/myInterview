fcbe8635039430cb12d7548037eb48d51c2c4ef6
pwang86 <pwang86@gmail.com>
2021-08-27 07:25:18 +1000

    Lowest Common Ancestor of a BST in python

--- python.py
+++ python.py
@@ -1070,6 +1070,17 @@ class Solution:
         if not left:
             return right
         return left
+    
+    # Lowest Common Ancestor of a Binary Search Tree
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
+        return None
 
 # Sliding Window Maximum
 class MyQueue:
