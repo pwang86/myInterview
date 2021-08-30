2218bdc97408c584fbd9b6c884b0e16a2ff8e09d
pwang86 <pwang86@gmail.com>
2021-08-30 22:54:28 +1000

    trim a bst in c sharp

--- csharp.cs
+++ csharp.cs
@@ -1390,6 +1390,20 @@ public class Solution {
         }
         return root;
     }
+
+    // trim a binary search tree
+    public TreeNode TrimBST(TreeNode root, int low, int high) {
+        if (root == null) return root;
+        if (root.val < low) {
+            return TrimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return TrimBST(root.left, low, high);
+        }
+        root.left = TrimBST(root.left, low, high);
+        root.right = TrimBST(root.right, low, high);
+        return root;
+    }
 } 
 
 // implement queue using stacks
