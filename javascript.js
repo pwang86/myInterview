0434d18ccfabee2c3da257b8b94d99913da2d72c
pwang86 <pwang86@gmail.com>
2021-08-30 22:54:39 +1000

    trim a bst in js

--- javascript.js
+++ javascript.js
@@ -1388,6 +1388,25 @@ var buildTree = function(preorder, inorder) {
     }
     return root;
 };
+// trim a binary search tree
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {TreeNode}
+ */
+ var trimBST = function(root, low, high) {
+    if (root == null) return root;
+    if (root.val < low) {
+        return trimBST(root.right, low, high);
+    }
+    if (root.val > high) {
+        return trimBST(root.left, low, high);
+    }
+    root.left = trimBST(root.left, low, high);
+    root.right = trimBST(root.right, low, high);
+    return root;
+};
 
 // implement queue using stacks
 /**
