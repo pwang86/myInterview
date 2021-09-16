621b1f7967fd2c174e00f9009488bd6152c074a1
pwang86 <pwang86@gmail.com>
2021-09-16 22:06:21 +1000

    update Reconstruct Itinerary in python

--- python.py
+++ python.py
@@ -1381,32 +1381,32 @@ class Solution:
     
     # Reconstruct Itinerary
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        # defaultdic(list) 是为了方便直接append
-        tickets_dict = defaultdict(list)
-        for item in tickets:
-            tickets_dict[item[0]].append(item[1])
-        '''
-        tickets_dict里面的内容是这样的
-         {'JFK': ['SFO', 'ATL'], 'SFO': ['ATL'], 'ATL': ['JFK', 'SFO']})
-        '''
-        path = ["JFK"]
-        def backtracking(start_point):
-            # 终止条件
-            if len(path) == len(tickets) + 1:
+        res = []
+        myMap = defaultdict(list)
+        res.append("JFK")
+        for t in tickets:
+            myMap[t[0]].append(t[1])
+        for m in myMap.values():
+            m.sort()
+        def backtrack(start: str) -> bool:
+            if len(res) == len(tickets) + 1:
                 return True
-            tickets_dict[start_point].sort()
-            for _ in tickets_dict[start_point]:
-                #必须及时删除，避免出现死循环
-                end_point = tickets_dict[start_point].pop(0)
-                path.append(end_point)
-                # 只要找到一个就可以返回了
-                if backtracking(end_point):
+            if start not in myMap:
+                return False
+            if len(myMap[start]) == 0:
+                return False
+            for i in range(len(myMap[start])):
+                city = myMap[start][i]
+                myMap[start].pop(i)
+                res.append(city)
+                if backtrack(city):
                     return True
-                path.pop()
-                tickets_dict[start_point].append(end_point)
-
-        backtracking("JFK")
-        return path
+                res.pop()
+                myMap[start].insert(i, city)
+            return False
+        
+        backtrack("JFK")
+        return res
        
 # Sliding Window Maximum
 class MyQueue:
