9ef2c93d8e0073db88cae86b75c5e2f7f3cb7db3
pwang86 <pwang86@gmail.com>
2021-09-17 21:41:10 +1000

    N queens  in python

--- python.py
+++ python.py
@@ -1407,6 +1407,46 @@ class Solution:
         
         backtrack("JFK")
         return res
+    
+    # N queens
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board = [['.'] * n for _ in range(n)]
+        def isValid(row, col, board) -> bool:
+            for i in range(n):
+                if board[i][col] == "Q":
+                    return False
+            i = row - 1
+            j = col - 1
+            while i >= 0 and j >= 0:
+                if board[i][j] == "Q":
+                    return False
+                i -= 1
+                j -= 1
+            i = row - 1
+            j = col + 1
+            while i >= 0 and j < n:
+                if board[i][j] == "Q":
+                    return False
+                i -= 1
+                j += 1
+            return True
+        def toList(board) -> List[str]:
+            tmp = []
+            for i in range(n):
+                tmp.append("".join(board[i]))
+            return tmp
+        def backtrack(row: int):
+            if row == n:
+                res.append(toList(board))
+                return
+            for col in range(n):
+                if isValid(row, col, board):
+                    board[row][col] = 'Q'
+                    backtrack(row + 1)
+                    board[row][col] = '.'
+        backtrack(0)
+        return res
        
 # Sliding Window Maximum
 class MyQueue:
