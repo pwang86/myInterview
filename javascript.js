02dcc7bd8d6a03bdad35468732bdc49094de1a8b
pwang86 <pwang86@gmail.com>
2021-09-17 21:41:03 +1000

    N queens  in js

--- javascript.js
+++ javascript.js
@@ -1779,6 +1779,67 @@ var partition = function(s) {
     return res;
 };
 
+// N queens
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+ var solveNQueens = function(n) {
+    let res = [];
+    let board = new Array(n);
+    for (let i = 0; i < n; i++) {
+        board[i] = new Array(n);
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            board[i][j] = ".";
+        }
+    }
+    const isValid = (row, col, board) => {
+        for (let i = 0 ; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    };
+    
+    const arrToList = board => {
+        let list = [];
+        for (let i = 0; i < n; i++) {
+            list.push(board[i].join(""));
+        }
+        return list;
+    };
+    
+    const backtrack = row => {
+        if (row == n) {
+            res.push(arrToList(board));
+            return;
+        }
+        for (let col = 0; col < n; col++) {
+            if (isValid(row, col, board)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1);
+                board[row][col] = '.';
+            }
+        }
+    };
+    
+    backtrack(0);
+    return res;
+};
+
 // implement queue using stacks
 /**
  * Initialize your data structure here.
