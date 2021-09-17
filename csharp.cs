5575762acf5e66fd9c9ed48d49d088ffe0d465a7
pwang86 <pwang86@gmail.com>
2021-09-17 21:40:56 +1000

    N queens  in c sharp

--- csharp.cs
+++ csharp.cs
@@ -1745,6 +1745,60 @@ public class Solution {
         }
         return false;
     }
+
+    // N queens
+    public IList<IList<string>> res = new List<IList<string>>();
+    public IList<IList<string>> SolveNQueens(int n) {
+        char[][] board = new char[n][];
+        for (int i = 0; i < n; i++) {
+            board[i] = new char[n];
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        Backtrack(board, 0, n);
+        return res;
+    }
+    public void Backtrack(char[][] board, int row, int n) {
+        if (row == n) {
+            res.Add(ArrToList(board));
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (IsValid(row, col, n, board)) {
+                board[row][col] = 'Q';
+                Backtrack(board, row + 1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    public bool IsValid(int row, int col, int n, char[][] board) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public IList<string> ArrToList(char[][] board) {
+        IList<string> list = new List<string>();
+        for (int i = 0; i < board[0].Length; i++) {
+            list.Add(string.Join("", board[i]));
+        }
+        return list;
+    }
 } 
 
 // implement queue using stacks
