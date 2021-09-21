eefa4c1e65c985110d4c97901cdca111a9ee84aa
pwang86 <pwang86@gmail.com>
2021-09-22 06:36:29 +1000

    Best Time to Buy and Sell Stock II in python

--- python.py
+++ python.py
@@ -1518,7 +1518,14 @@ class Solution:
             if count <= 0:
                 count = 0
         return res
-       
+    
+    # Best Time to Buy and Sell Stock II
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        for i in range(1, len(prices)):
+            res += max(prices[i] - prices[i - 1], 0)
+        return res
+
 # Sliding Window Maximum
 class MyQueue:
     def __init__(self):
