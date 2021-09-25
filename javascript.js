d80234e4fda6151cbd262fbb718e255ee5421272
pwang86 <pwang86@gmail.com>
2021-09-26 09:19:26 +1000

    Gas Station in js

--- javascript.js
+++ javascript.js
@@ -1967,6 +1967,28 @@ var partition = function(s) {
     return res;
 };
 
+// Gas Station
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+ var canCompleteCircuit = function(gas, cost) {
+    let start = 0;
+    let totalSum = 0;
+    let curSum = 0;
+    for (let i = 0; i < gas.length; i++) {
+        curSum += gas[i] - cost[i];
+        totalSum += gas[i] - cost[i];
+        if (curSum < 0) {
+            curSum = 0;
+            start = i + 1;
+        }
+    }
+    if (totalSum < 0) return -1;
+    return start;
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
