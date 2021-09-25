dfe8d2423439faac14e9eec67725e1976e53bf7e
pwang86 <pwang86@gmail.com>
2021-09-25 12:09:11 +1000

    Maximize Sum Of Array After K Negations in js

--- javascript.js
+++ javascript.js
@@ -1967,6 +1967,26 @@ var partition = function(s) {
     return res;
 };
 
+// Maximize Sum Of Array After K Negations
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var largestSumAfterKNegations = function(nums, k) {
+    nums.sort((a, b) => Math.abs(b) - Math.abs(a));
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < 0 && k > 0) {
+            nums[i] = -nums[i];
+            k--;
+        }
+    }
+
+    if (k % 2 == 1) nums[nums.length - 1] = -nums[nums.length - 1];
+    return nums.reduce((a,b) => a + b);
+};
+
 // Maximum Subarray
 /**
  * @param {number[]} nums
