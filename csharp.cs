cacb61c89b6577b6b1dc1f168314a3a4954fd3a5
pwang86 <pwang86@gmail.com>
2021-09-26 09:19:18 +1000

    Gas Station in c sharp

--- csharp.cs
+++ csharp.cs
@@ -1932,6 +1932,23 @@ public class Solution {
         if (k % 2 == 1) arr[0] = -arr[0];
         return arr.Sum();
     }
+
+    // Gas Station
+     public int CanCompleteCircuit(int[] gas, int[] cost) {
+        int start = 0;
+        int curSum = 0;
+        int totalSum = 0;
+        for (int i = 0; i < gas.Length; i++) {
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if (curSum < 0) {
+                curSum = 0;
+                start = i + 1;
+            }
+        }
+        if (totalSum < 0) return -1;
+        return start;
+    }
 } 
 
 // implement queue using stacks
