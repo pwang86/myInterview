afa45717a1608fe153059145b0c09a2aa3b13abf
pwang86 <pwang86@gmail.com>
2021-09-26 09:19:33 +1000

    Gas Station in python

--- python.py
+++ python.py
@@ -1562,6 +1562,20 @@ class Solution:
         if k % 2 == 1:
             arr[0] *= -1
         return sum(arr)
+    
+    # Gas Station
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = 0
+        curSum = totalSum = 0
+        for i in range(len(gas)):
+            curSum += (gas[i] - cost[i])
+            totalSum += (gas[i] - cost[i])
+            if curSum < 0:
+                curSum = 0
+                start = i + 1
+        if totalSum < 0:
+            return -1
+        return start
 
 # Sliding Window Maximum
 class MyQueue:
