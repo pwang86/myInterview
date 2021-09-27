601c60c660f457b9025bdac29b5ac4a0f788513e
pwang86 <pwang86@gmail.com>
2021-09-28 07:20:31 +1000

    Lemonade Change in js

--- javascript.js
+++ javascript.js
@@ -2010,6 +2010,35 @@ var partition = function(s) {
     return res;
 };
 
+// Lemonade Change
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+ var lemonadeChange = function(bills) {
+    let five = 0;
+    let ten = 0;
+    for (let i = 0; i < bills.length; i++) {
+        if (bills[i] == 5) {
+            five++;
+        } else if (bills[i] == 10) {
+            if (five <= 0) return false;
+            five--;
+            ten++;
+        } else {
+            if (five > 0 && ten > 0) {
+                five--;
+                ten--;
+            } else if (five >= 3) {
+                five -=3;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
