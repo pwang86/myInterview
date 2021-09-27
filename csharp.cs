fd1f26d3bbaec32fef6af9296caa1b566eb86006
pwang86 <pwang86@gmail.com>
2021-09-28 07:20:24 +1000

    Lemonade Change

--- csharp.cs
+++ csharp.cs
@@ -1966,6 +1966,31 @@ public class Solution {
         }
         return candies.Sum();
     }
+
+    // Lemonade Change
+    public bool LemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int i = 0; i < bills.Length; i++) {
+            if (bills[i] == 5) {
+                five++;
+            } else if (bills[i] == 10) {
+                if (five <= 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 } 
 
 // implement queue using stacks
