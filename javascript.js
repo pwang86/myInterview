96f2af1338e44f95740ed9ae24f58d51c65ede67
pwang86 <pwang86@gmail.com>
2021-09-29 07:44:09 +1000

    Queue Reconstruction by Height in js

--- javascript.js
+++ javascript.js
@@ -2039,6 +2039,25 @@ var partition = function(s) {
     return true;
 };
 
+// Queue Reconstruction by Height
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+ var reconstructQueue = function(people) {
+    people.sort((a, b) => {
+        if (a[0] == b[0]) {
+            return a[1] - b[1]; 
+        }
+        return b[0] - a[0];
+    });
+    let res = [];
+    for (let i = 0; i < people.length; i++) {
+        res.splice(people[i][1], 0, people[i]);
+    }
+    return res;
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
