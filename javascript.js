118d18536c8e9c022e434b402ded8c5dc3b06336
pwang86 <pwang86@gmail.com>
2021-09-30 07:14:38 +1000

    Minimum Number of Arrows to Burst Balloons in js

--- javascript.js
+++ javascript.js
@@ -2058,6 +2058,24 @@ var partition = function(s) {
     return res;
 };
 
+// Minimum Number of Arrows to Burst Balloons
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+ var findMinArrowShots = function(points) {
+    points.sort((a, b) => a[0] - b[0]);
+    let count = 1
+    for (let i = 1; i < points.length; i++) {
+        if (points[i][0] > points[i - 1][1]) {
+            count++;
+        } else {
+            points[i][1] = Math.min(points[i - 1][1], points[i][1]);
+        }
+    }
+    return count;
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
