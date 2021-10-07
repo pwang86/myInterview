6ab52558d4bbdbb1e619741bea452d2dd07fa25d
pwang86 <pwang86@gmail.com>
2021-10-08 07:10:59 +1100

    Fibonacci Number in c sharp

--- csharp.cs
+++ csharp.cs
@@ -2127,6 +2127,20 @@ public class Solution {
             return 2;
         }
     }
+
+    // Fibonacci Number
+    public int Fib(int n) {
+        if (n <= 1) return n;
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int sum = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = sum;
+        }
+        return dp[1];
+    }
 } 
 
 // implement queue using stacks
