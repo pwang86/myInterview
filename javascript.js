36761a9a0c2a600efd5d4baa00103e1926008709
pwang86 <pwang86@gmail.com>
2021-10-08 07:11:07 +1100

    Fibonacci Number in js

--- javascript.js
+++ javascript.js
@@ -2219,6 +2219,24 @@ var partition = function(s) {
     return res;
 };
 
+// Fibonacci Number
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var fib = function(n) {
+    if (n <= 1) return n;
+    let dp = new Array(2);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        let sum = dp[0] + dp[1];
+        dp[0] = dp[1];
+        dp[1] = sum;
+    }
+    return dp[1];
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
