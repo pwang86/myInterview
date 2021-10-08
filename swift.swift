d614adb5941a90881b98645e6615f25462991b4f
pwang86 <pwang86@gmail.com>
2021-10-09 08:34:20 +1100

    Climbing Stairs in swift

--- swift.swift
+++ swift.swift
@@ -2240,6 +2240,21 @@ class solution {
         }
         return dp[1]
     }
+
+    // Climbing Stairs
+    func climbStairs(_ n: Int) -> Int {
+        if n <= 2 {
+            return n
+        }
+        var a = 1
+        var b = 2
+        for i in 3..<n+1 {
+            var mySum = a + b
+            a = b
+            b = mySum
+        }
+        return b
+    }
 }
 
 // Restore IP Addresses
