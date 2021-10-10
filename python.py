278f678983d80c9b2330d71b15f7f6bcdc33a06c
pwang86 <pwang86@gmail.com>
2021-10-11 06:57:15 +1100

    Unique Paths in python

--- python.py
+++ python.py
@@ -1748,6 +1748,15 @@ class Solution:
             a = b
             b = mySum
         return min(a, b)
+    
+    # Unique Paths
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for j in range(n)] for i in range(m)]
+        # print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
 
 # Sliding Window Maximum
 class MyQueue:
