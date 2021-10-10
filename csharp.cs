50a22c8138ba6a4bb1f38a920e647099f81838bf
pwang86 <pwang86@gmail.com>
2021-10-11 06:57:01 +1100

    Unique Paths in c sharp

--- csharp.cs
+++ csharp.cs
@@ -2165,6 +2165,26 @@ public class Solution {
         }
         return Math.Min(a, b);
     }
+
+    // Unique Paths
+    public int UniquePaths(int m, int n) {
+        int[][] dp = new int[m][];
+        for (int i = 0; i < m; i++) {
+            dp[i] = new int[n];
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 } 
 
 // implement queue using stacks
