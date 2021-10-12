f55d21de8e7f2be81316c56b2ad04b64a36d64dd
pwang86 <pwang86@gmail.com>
2021-10-12 22:49:18 +1100

    Unique Paths II in python

--- python.py
+++ python.py
@@ -1757,6 +1757,28 @@ class Solution:
             for j in range(1, n):
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         return dp[m - 1][n - 1]
+    
+    # Unique Paths II
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = 1
+        for i in range(1, m):
+            if obstacleGrid[i][0] == 0 and dp[i - 1][0] == 1:
+                dp[i][0] = 1
+                
+        for i in range(1, n):
+            if obstacleGrid[0][i] == 0 and dp[0][i - 1] == 1:
+                dp[0][i] = 1
+        print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 0:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
 
 # Sliding Window Maximum
 class MyQueue:
