219db83e292d200c0a2a2a2eaf0e7cfa75712c2a
pwang86 <pwang86@gmail.com>
2021-10-13 07:18:47 +1100

    Integer Break in js

--- javascript.js
+++ javascript.js
@@ -2315,6 +2315,22 @@ var partition = function(s) {
     return dp[m - 1][n - 1];
 };
 
+// Integer Break
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var integerBreak = function(n) {
+    let dp = new Array(n + 1).fill(0);
+    dp[2] = 1;
+    for (let i = 3; i <= n; i++) {
+        for (let j = 1; j < i - 1; j++) {
+            dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+        }
+    }
+    return dp[n];
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
