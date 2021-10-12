59bc4fc956ef8d200b7e02f2d1ee58c5e570ca0a
pwang86 <pwang86@gmail.com>
2021-10-12 22:49:26 +1100

    Unique Paths II in swift

--- swift.swift
+++ swift.swift
@@ -2277,7 +2277,40 @@ class solution {
             }
         }
         return dp[m - 1][n - 1]
-    } 
+    }
+
+    // Unique Paths II
+    func uniquePathsWithObstacles(_ obstacleGrid: [[Int]]) -> Int {
+        if obstacleGrid[0][0] == 1 {
+            return 0
+        }
+        let m = obstacleGrid.count
+        let n = obstacleGrid[0].count
+        var dp = Array(repeating: Array(repeating: 0, count: n), count: m)
+        
+        dp[0][0] = 1
+        
+        for i in 1..<m {
+            if obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1 {
+                dp[i][0] = 1
+            }
+        }
+        
+        for i in 1..<n {
+            if obstacleGrid[0][i] == 0 && dp[0][ i - 1] == 1 {
+                dp[0][i] = 1
+            }
+        }
+        
+        for i in 1..<m {
+            for j in 1..<n {
+                if obstacleGrid[i][j] == 0 {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+                }
+            }
+        }
+        return dp[m - 1][n - 1]
+    }
 }
 
 // Restore IP Addresses
