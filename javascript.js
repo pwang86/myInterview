0eecc87211206065b9a4d2957c3270a966240f56
pwang86 <pwang86@gmail.com>
2021-10-14 07:21:57 +1100

    Unique BST in js

--- javascript.js
+++ javascript.js
@@ -2331,6 +2331,23 @@ var partition = function(s) {
     return dp[n];
 };
 
+// Unique Binary Search Trees
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var numTrees = function(n) {
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+    }
+    return dp[n];
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
