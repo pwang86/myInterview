cf1ef73243eb71080a3249c3f2984b03416210ce
pwang86 <pwang86@gmail.com>
2021-10-14 07:21:49 +1100

    Unique BST in c sharp

--- csharp.cs
+++ csharp.cs
@@ -2220,6 +2220,19 @@ public class Solution {
         }
         return dp[n];
     }
+
+    // Unique Binary Search Trees
+    public int NumTrees(int n) {
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        res[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                res[i] += res[j - 1] * res[i - j];
+            }
+        }
+        return res[n];
+    }
 } 
 
 // implement queue using stacks
