290a6ede4752df1185012c5c60303751e796b0b2
pwang86 <pwang86@gmail.com>
2021-10-14 07:22:11 +1100

    Unique BST in swift

--- swift.swift
+++ swift.swift
@@ -2323,6 +2323,19 @@ class solution {
         }
         return dp[n]
     }
+
+    // Unique Binary Search Trees
+    func numTrees(_ n: Int) -> Int {
+        var dp = Array(repeating: 0, count: n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in 2..<n+1 {
+            for j in 1..<i+1  {
+                dp[i] += dp[j - 1] * dp[i - j]
+            }
+        }
+        return dp[n]
+    }
 }
 
 // Restore IP Addresses
