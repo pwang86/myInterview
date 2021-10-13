7f5155bccfc8c95bd824a1c82166a01cb1ea704f
pwang86 <pwang86@gmail.com>
2021-10-14 07:22:04 +1100

    Unique BST in python

--- python.py
+++ python.py
@@ -1788,6 +1788,16 @@ class Solution:
             for j in range(1, i - 1):
                 dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]))
         return dp[n]
+    
+    # Unique Binary Search Trees
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        return dp[n]
 
 # Sliding Window Maximum
 class MyQueue:
