8aaa5b8cebe9029dcd1b8920db2cdfe4ad8295de
pwang86 <pwang86@gmail.com>
2021-10-16 16:19:21 +1100

    Last Stone Weight II in python

--- python.py
+++ python.py
@@ -1813,6 +1813,16 @@ class Solution:
             for j in range(target, nums[i] - 1, -1):
                 dp[j] = max(dp[j], dp[j - nums[i]] + nums[i])
         return dp[target] == target
+    
+    # Last Stone Weight II
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        mySum = sum(stones)
+        target = mySum // 2
+        dp = [0] * (target + 1)
+        for i in range(len(stones)):
+            for j in range(target, stones[i] - 1, -1):
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i])
+        return mySum - 2 * dp[target]
 
 # Sliding Window Maximum
 class MyQueue:
