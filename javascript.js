aba643adbea20fbf25167052ac978efa229b784f
pwang86 <pwang86@gmail.com>
2021-10-16 16:19:14 +1100

    Last Stone Weight II in js

--- javascript.js
+++ javascript.js
@@ -2368,6 +2368,23 @@ var partition = function(s) {
     return dp[target] == target;
 };
 
+// Last Stone Weight II
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+ var lastStoneWeightII = function(stones) {
+    let sum = stones.reduce((a, b) => a + b, 0);
+    let target = Math.floor(sum / 2);
+    let dp = new Array(target + 1).fill(0);
+    for (let i = 0; i < stones.length; i++) {
+        for (let j = target; j >= stones[i]; j--) {
+            dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i]);
+        }
+    }
+    return sum - 2 * dp[target];
+};
+
 // Maximize Sum Of Array After K Negations
 /**
  * @param {number[]} nums
