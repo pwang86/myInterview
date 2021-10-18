91433f0f74a863c572eb7d1b1911d7a9e366f037
pwang86 <pwang86@gmail.com>
2021-10-18 22:01:00 +1100

    Ones and Zeroes in c sharp

--- csharp.cs
+++ csharp.cs
@@ -2277,6 +2277,28 @@ public class Solution {
             }
         }
         return dp[size];
+    }
+
+    // Ones and Zeroes
+    public int FindMaxForm(string[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][];
+        for (int i = 0; i < m + 1; i++) {
+            dp[i] = new int[n + 1];
+        }
+        
+        for (int k = 0; k < strs.Length; k++) {
+            int zero = 0, one = 0;
+            foreach (char ch in strs[k]) {
+                if (ch == '0') zero++;
+                else one++;
+            }
+            for (int i = m; i >= zero; i--) {
+                for (int j = n; j >= one; j--) {
+                    dp[i][j] = Math.Max(dp[i][j], dp[i - zero][j - one] + 1);
+                }
+            }
+        }
+        return dp[m][n];
     } 
 } 
 
